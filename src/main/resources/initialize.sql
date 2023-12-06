481296c891803c592a855cf32a0916587c3bad1f
ArmanKamal <armankamal2421@gmail.com>
2023-12-05 20:25:16 -0600

    Updated table schema table

--- src/main/resources/initialize.sql
+++ src/main/resources/initialize.sql
@@ -1,81 +1,116 @@
 -- Table for Persons
-CREATE TABLE IF NOT EXISTS Persons
+CREATE TABLE Persons
 (
     university_id VARCHAR(20) PRIMARY KEY,
-    person_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL
+    person_name   VARCHAR(255)        NOT NULL,
+    email         VARCHAR(255) UNIQUE NOT NULL
 );
 
 -- Table for Departments
-CREATE TABLE IF NOT EXISTS Departments (
-    department_code VARCHAR(4) PRIMARY KEY,
-    department_name VARCHAR(255) UNIQUE NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS Programs
+CREATE TABLE Departments
 (
-    program_name   VARCHAR(255) PRIMARY KEY,
-    university_id VARCHAR(20),
-    department_code VARCHAR(4),
-    FOREIGN KEY (university_id) UNIQUE REFERENCES Persons(university_id)
-    FOREIGN KEY (department_code) REFERENCES Departments(department_code)
+    department_code       CHAR(4) PRIMARY KEY,
+    department_name       VARCHAR(255) UNIQUE NOT NULL,
+    head_of_department_id VARCHAR(20), -- Reference to Persons table
+    FOREIGN KEY (head_of_department_id) REFERENCES Persons (university_id)
 );
 
-
-CREATE TABLE FacultyMembers (
-    faculty_id INT PRIMARY KEY,
-    faculty_name VARCHAR(255) NOT NULL,
-    email_address VARCHAR(255) NOT NULL,
-    rank enum ('full', 'associate', 'assistant', 'adjunct') NOT NULL,
-    department_code CHAR(4) NOT NULL,
-    FOREIGN KEY (department_code) REFERENCES Departments(department_code)
+-- Table for Programs
+CREATE TABLE Programs
+(
+    program_id         INT PRIMARY KEY,
+    program_name       VARCHAR(255) UNIQUE NOT NULL,
+    department_code    CHAR(4),
+    head_of_program_id VARCHAR(20), -- Reference to Persons table
+    FOREIGN KEY (department_code) REFERENCES Departments (department_code),
+    FOREIGN KEY (head_of_program_id) REFERENCES Persons (university_id)
 );
 
-CREATE TABLE Courses (
-     course_id CHAR(8) PRIMARY KEY,
-     course_title VARCHAR(255) NOT NULL,
-     course_description TEXT,
-     department_code CHAR(4) NOT NULL,
-     FOREIGN KEY (department_code) REFERENCES Departments(department_code)
+-- Table for FacultyMembers
+CREATE TABLE FacultyMembers
+(
+    faculty_id      INT PRIMARY KEY,
+    faculty_name    VARCHAR(255) NOT NULL,
+    email_address   VARCHAR(255) NOT NULL,
+    rank            ENUM('full', 'associate', 'assistant', 'adjunct') NOT NULL,
+    department_code CHAR(4),
+    FOREIGN KEY (department_code) REFERENCES Departments (department_code)
 );
 
-CREATE TABLE CourseOfferings (
-     offering_id INT PRIMARY KEY,
-     course_id CHAR(8) NOT NULL,
-     semester enum('Fall','Spring','Summer') NOT NULL,
-     section_number UNIQUE INT NOT NULL, /* Auto increment 001 - 999 */
-     faculty_id INT NOT NULL,
-     enrolled_students INT NOT NULL,
-     PRIMARY KEY (course_id, semester, section_number),
-     FOREIGN KEY (course_id) REFERENCES Courses(course_id),
-     FOREIGN KEY (faculty_id) REFERENCES FacultyMembers(faculty_id)
-);
--- Table for learning objectives
-CREATE TABLE LearningObjective (
-                                   ObjectiveCode VARCHAR(20) PRIMARY KEY,
-                                   Description TEXT NOT NULL
+-- Table for Courses
+CREATE TABLE Courses
+(
+    course_id          CHAR(8) PRIMARY KEY,
+    course_title       VARCHAR(255) NOT NULL,
+    course_description TEXT,
+    department_code    CHAR(4),
+    PRIMARY KEY (course_id),
+    FOREIGN KEY (department_code) REFERENCES Departments (department_code)
 );
 
--- Table for subobjectives
-CREATE TABLE SubObjective (
-                              ObjectiveCode VARCHAR(20),
-                              SubObjectiveCode INT,
-                              Description TEXT NOT NULL,
-                              PRIMARY KEY (ObjectiveCode, SubObjectiveCode),
-                              FOREIGN KEY (ObjectiveCode) REFERENCES LearningObjective(ObjectiveCode)
+-- Table for Sections
+CREATE TABLE Sections
+(
+    offering_id       INT PRIMARY KEY,
+    course_id         CHAR(8)    NOT NULL,
+    semester          ENUM('Fall','Spring','Summer') NOT NULL,
+    section_number    INT UNIQUE NOT NULL,
+    faculty_id        INT        NOT NULL,
+    enrolled_students INT        NOT NULL,
+    PRIMARY KEY (offering_id),
+    FOREIGN KEY (course_id) REFERENCES Courses (course_id),
+    FOREIGN KEY (faculty_id) REFERENCES FacultyMembers (faculty_id)
 );
 
+-- Table for LearningObjectives
+CREATE TABLE LearningObjectives
+(
+    objective_code VARCHAR(20) PRIMARY KEY,
+    description    TEXT NOT NULL
+);
 
--- Table for program-learning objective relationships
-CREATE TABLE ProgramObjective (
-                                  program_name VARCHAR(255),
-                                  course_id CHAR(8) NOT NULL,
-                                  objective_code VARCHAR(20),
-                                  sub_objective_code VARCHAR(20),
-                                   num_met INT,
-                                  PRIMARY KEY (ProgramName, ObjectiveCode),
-                                  FOREIGN KEY (ProgramName) REFERENCES Program(ProgramName),
-                                  FOREIGN KEY (course_id) REFERENCES Courses(course_id),
-                                  FOREIGN KEY (objective_code) REFERENCES LearningObjective(objective_code),
-                                  FOREIGN KEY (sub_objective_code) REFERENCES SubObjective(sub_objective_code)
+-- Table for SubObjectives
+CREATE TABLE SubObjectives
+(
+    sub_objective_code INT PRIMARY KEY,
+    objective_code     VARCHAR(20),
+    description        TEXT NOT NULL,
+    PRIMARY KEY (objective_code, sub_objective_code),
+    FOREIGN KEY (objective_code) REFERENCES LearningObjectives (objective_code)
+);
+
+-- Table for ProgramCourses
+CREATE TABLE ProgramCourses
+(
+    program_id INT,
+    course_id  CHAR(8),
+    PRIMARY KEY (program_id, course_id),
+    FOREIGN KEY (program_id) REFERENCES Programs (program_id),
+    FOREIGN KEY (course_id) REFERENCES Courses (course_id)
+);
+
+-- Table for ProgramObjectives
+CREATE TABLE ProgramObjectives
+(
+    program_id         INT,
+    course_id          CHAR(8),
+    objective_code     VARCHAR(20),
+    sub_objective_code INT,
+    PRIMARY KEY (program_id, course_id, objective_code, sub_objective_code),
+    FOREIGN KEY (program_id) REFERENCES Programs (program_id),
+    FOREIGN KEY (course_id) REFERENCES Courses (course_id),
+    FOREIGN KEY (objective_code) REFERENCES LearningObjectives (objective_code),
+    FOREIGN KEY (sub_objective_code) REFERENCES SubObjectives (sub_objective_code)
+);
+
+-- Table for EvaluationResults
+CREATE TABLE EvaluationResults
+(
+    offering_id        INT PRIMARY KEY,
+    sub_objective_code INT,
+    evaluation_method  VARCHAR(50) NOT NULL,
+    students_met       INT         NOT NULL,
+    PRIMARY KEY (offering_id),
+    FOREIGN KEY (sub_objective_code) REFERENCES SubObjectives (sub_objective_code),
+    FOREIGN KEY (offering_id) REFERENCES Sections (offering_id)
 );
